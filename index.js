c8d7a1e9a1064c2f4ea363e418fc321942096cd6
Natali kruk <natasha_505@yahoo.com>
2019-07-10 22:44:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
+let dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"];
 function dwarfRollCall(dwarves) {
+  var numberedNames = ''; // created empty string OUTSIDE to run
+  for (let i = 0; i < dwarves.length; i++) {
+    let currentName = dwarves[i]; // indexed Names w/#'s'
+    let currentNumber = i +1; // cuz starts with 0
+    numberedNames += `${currentNumber}. ${currentName} `; // if only = will erase what's before, += keeps adding to each loop
+        if(currentNumber != dwarves.length){
+      numberedNames += '';
+    }
+  }
+numberedNames += '';
+return numberedNames;
 }
+
 
 function summonCaptainPlanet(planeteerCalls){
 }
 
+
 function longPlaneteerCalls(words) {
 }
 
+
 function findTheCheese (foods) {
 }
+
+
+
+console.log(numberedNames);
+
+console.log("currentNumber: " + currentNumber + ", dwarves.length: " + dwarves.length)
+
